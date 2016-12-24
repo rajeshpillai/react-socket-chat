8938f7726761bc81631ee3a5836e1cdfa16f82dc
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-24 13:36:53 +0530

    Setup basic react component.  NOTE: Set type to text/babel for react components

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+  app = express(),
+  server = require("http").createServer(app);
+
+var io = require("socket.io").listen(server);
+
+
+var port = process.env.PORT || 9999;
+
+app.use("/clientapp", express.static(__dirname + '/clientapp'));
+
+
+io.on("connection", function (socket) {
+  console.log("We have a connection...");
+  socket.on("new-message", function (msg) {
+    console.log(msg);
+    io.emit("receive-message", msg);
+  })
+});
+
+server.listen(port, function () {
+  console.log("Connected on port ", port);
+
+});
+
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
