20786864e066515f13b7c986a69fd089e9442e18
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-24 13:41:26 +0530

    Added reference to socket.io.js

--- clientapp/chat-component.js
+++ clientapp/chat-component.js
@@ -1,4 +1,13 @@
 var ChatApp = React.createClass({
+  getInitialState: function () {
+    return {
+      messages: [],
+      socket: io('http://localhost:9999')
+    }
+  },
+  componentDidMount: function () {
+
+  },
   render: function () {
     return ( < div >
       This will be the chat application < /div>
