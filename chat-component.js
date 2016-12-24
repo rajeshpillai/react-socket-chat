6ac3c7cf2c6aee52fe5cdbb5034c3007ee44cfb8
Rajesh Pillai <pillai.rajesh@gmail.com>
2016-12-24 13:17:44 +0530

    Setup for chat app

---
+++ chat-component.js
@@ -0,0 +1,11 @@
+var ChatApp = React.createClass({
+  render: function () {
+    return ( < div >
+      This will be the chat application < /div>
+    )
+  }
+});
+
+ReactDOM.render( < ChatApp / > ,
+  document.getElementById("chat")
+)
